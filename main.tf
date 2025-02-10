c7fcc5f0b54ff1872174be7cac64aea6b19d4bfb
siddharth7411 <siddharth7411@gmail.com>
2025-02-10 12:02:15 -0500

    first commit of project8

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-terraworks"
+  location = "West Europe"
+}
+
+resource "azuread_user" "p8" {
+  user_principal_name = "p8@pemmrajusirishaoutlook.onmicrosoft.com"  # Replace with your tenant's verified domain
+  display_name        = "P8 User"
+  password            = var.admin_password
+}
+
+resource "azurerm_role_assignment" "example" {
+  principal_id         = azuread_user.p8.object_id
+  role_definition_name = "Contributor"
+  scope                = "/subscriptions/${var.subscription_id}"
+}
