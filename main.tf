c2ea7963bec201275d44eb90a64fe02821a207f3
siddharth7411 <siddharth7411@gmail.com>
2025-02-11 15:13:16 -0500

    remodified

--- main.tf
+++ main.tf
@@ -12,29 +12,19 @@ provider "azuread" {
   tenant_id       = var.tenant_id
 }
 
-# Commented out as they are already created and imported
-# resource "azurerm_resource_group" "example" {
-#   name     = "rg-terraworks"
-#   location = "West Europe"
-# }
+resource "azurerm_resource_group" "example" {
+  name     = "rg-terraworks"
+  location = "West Europe"
+}
 
-# Commented out as the user already exists
-# resource "azuread_user" "p8" {
-#   user_principal_name = "p8@pemmrajusirishaoutlook.onmicrosoft.com"
-#   display_name        = "P8 User"
-#   password            = var.admin_password
-# }
-
-data "azuread_user" "existing" {
-  object_id = "547708e3-7184-4b31-81f7-381bfa8acb5e"
+resource "azuread_user" "p8" {
+  user_principal_name = "p8@pemmrajusirishaoutlook.onmicrosoft.com"
+  display_name        = "P8 User"
+  password            = var.admin_password
 }
 
 resource "azurerm_role_assignment" "example" {
-  principal_id         = data.azuread_user.existing.object_id
+  principal_id         = azuread_user.p8.object_id
   role_definition_name = "Contributor"
   scope                = "/subscriptions/${var.subscription_id}"
-
-  lifecycle {
-    ignore_changes = [principal_id]
-  }
 }
